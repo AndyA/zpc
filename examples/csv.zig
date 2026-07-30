5629469a29936c8a58c70ed2393e655c2421a3de
Andy Armstrong <andy@hexten.net>
2026-07-30 13:36:22 +0100

    Don't swallow empty lines

--- examples/csv.zig
+++ examples/csv.zig
@@ -1,7 +1,5 @@
 const std = @import("std");
 const print = std.debug.print;
-const assert = std.debug.assert;
-const expectEqualDeep = std.testing.expectEqualDeep;
 
 const Io = std.Io;
 const Allocator = std.mem.Allocator;
@@ -9,12 +7,12 @@ const Allocator = std.mem.Allocator;
 const zpc = @import("zpc").Space(u8);
 
 const CsvTag = enum(u8) { NONE, QUOTED, BARE, ROW, CSV };
-
 const CsvContext = struct { allocator: Allocator };
 
 const P = zpc.Parsers(CsvContext, CsvTag);
 
 fn makeCsvParser() P.Parser {
+    // Skip horizontal whitespace
     const skipSpace = P.takeWhile(.NONE, .zeroOrMore, zpc.predAnd(
         std.ascii.isWhitespace,
         zpc.predNot(zpc.predSet("\r\n")),
@@ -31,22 +29,24 @@ fn makeCsvParser() P.Parser {
         P.literal("\""),
     );
 
-    const bareParser = P.span(
-        .BARE,
-        P.takeWhile(.NONE, .zeroOrMore, zpc.predNot(zpc.predSet("\",\r\n"))),
-    );
+    const validBare = zpc.predNot(zpc.predSet(",\r\n"));
+    const bareParser = P.span(.BARE, P.takeWhile(.NONE, .zeroOrMore, validBare));
 
     const valueParser = P.right(skipSpace, P.alt(&.{ stringParser, bareParser }));
 
-    const rowParser = P.advances(P.seq(.ROW, &.{
+    const rowParser = P.seq(.ROW, &.{
         valueParser,
         P.flat(P.many(.NONE, .zeroOrMore, P.right(
             P.right(skipSpace, P.literal(",")),
             valueParser,
         ))),
-    }));
+    });
 
-    const eolParser = P.takeWhile(.NONE, .oneOrMore, zpc.predSet("\r\n"));
+    const eolParser = P.alt(&.{
+        P.literal("\n"),
+        P.literal("\r\n"),
+        P.literal("\r"),
+    });
 
     const csvParser = P.seq(.CSV, &.{
         rowParser,
@@ -70,6 +70,8 @@ pub fn main(init: std.process.Init) !void {
     const ctx: CsvContext = .{ .allocator = init.gpa };
     const res = try jsonParser(ctx,
         \\"""Hello", "World""", Now
+        \\"
+        \\"
         \\1,2,3,4
     );
     defer res.deinit(ctx);
