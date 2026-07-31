66d8f25a3411cbd9caa8704068a08037f1c2ee0a
Andy Armstrong <andy@hexten.net>
2026-07-31 19:40:47 +0100

    Minor

--- examples/mule.zig
+++ examples/mule.zig
@@ -4,7 +4,7 @@ const assert = std.debug.assert;
 const expectEqualDeep = std.testing.expectEqualDeep;
 
 pub fn main(_: std.process.Init) !void {
-    const ints = &.{ u1, u2, u4, u8, u16, u32, u64, u128, u256, u512, u1024 };
+    const ints = &.{ u1, u2, u4, u8, u16, u32, u64, u128, u256, u512, u1024, f16, f32, f64 };
     inline for (ints) |int| {
         if (std.simd.suggestVectorLength(int)) |vlen| {
             print("Vector length for {s}: {d}\n", .{ @typeName(int), vlen });
