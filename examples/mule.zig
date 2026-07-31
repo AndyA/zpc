11b263bfc511fc41c11314c0983b5c9c9891b46c
Andy Armstrong <andy@hexten.net>
2026-07-31 19:18:57 +0100

    Maybe no vectors

--- examples/mule.zig
+++ examples/mule.zig
@@ -6,5 +6,7 @@ const expectEqualDeep = std.testing.expectEqualDeep;
 pub fn main(_: std.process.Init) !void {
     if (std.simd.suggestVectorLength(u8)) |vlen| {
         print("Vector length for u8: {d}\n", .{vlen});
+    } else {
+        print("No vectors for you\n", .{});
     }
 }
