d27a84dbc91a0adf631090395f6a521e8a83a71b
Andy Armstrong <andy@hexten.net>
2026-08-22 17:06:44 +0100

    deadbeef

--- src/zpc/comptime.zig
+++ src/zpc/comptime.zig
@@ -20,7 +20,7 @@ fn free(_: *anyopaque, _: []u8, _: Alignment, _: usize) void {
 }
 
 pub const non_allocator: Allocator = .{
-    .ptr = @ptrCast(@constCast(&alloc)),
+    .ptr = @ptrFromInt(0xdeadbeef),
     .vtable = &.{ .alloc = alloc, .resize = resize, .remap = remap, .free = free },
 };
 
